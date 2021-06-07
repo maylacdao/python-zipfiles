e89f1108b823becf3dcf68ef837e564e1663402b
maylacdao <may.lacdao@gmail.com>
2021-06-07 00:00:45 -0400

    Updates

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import requests
 import zipfile
 
 r = requests.get(
-    'https://drive.google.com/file/d/1dfGerWeWkcyQ9GX9x20rdSGj7WtEpzBB/view?usp=sharing')
+    'https://drive.google.com/u/0/uc?id=1dfGerWeWkcyQ9GX9x20rdSGj7WtEpzBB&export=download')
 
 with open('data.zip', 'wb') as f:
     f.write(r.content)
 
 with zipfile.ZipFile('data.zip', 'r') as data_zip:
     print(data_zip.namelist())
+    data_zip.extractall('data')
